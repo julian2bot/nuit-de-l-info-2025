7c0559a73a4f5919e81607ee067a4f4ae43f7bec
renaudin-clement <prof.renaudin.clement@gmail.com>
2025-12-04 22:26:52 +0100

    avancement

--- ndi_2025/resources/js/three.js
+++ ndi_2025/resources/js/three.js
@@ -1,12 +1,18 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
 
 // Exemple cube
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
+const center = new THREE.Vector3(0, 0, 0);
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+
 
 // OrbitControls
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -20,12 +26,46 @@ const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(5,5,5);
 scene.add(light);
 
-camera.position.set(2,2,5);
+camera.position.set(0, 10, 0);
+camera.lookAt(center);
+
+const loader = new GLTFLoader();
+
+let objet1;
+let objet2;
+
+loader.load( '/model/boite.glb', function ( gltf ) {
+
+  objet1 = gltf.scene;
+  scene.add( objet1 );
+
+}, undefined, function ( error ) {
+
+  console.error( error );
+
+} );
+
+loader.load( '/model/fruit.glb', function ( gltf ) {
+  objet2=gltf.scene;
+  objet2.scale.set(0.5, 0.5, 0.5);
+  objet2.position.x =1;
+  scene.add( objet2 );
+
+}, undefined, function ( error ) {
+
+  console.error( error );
+
+} );
 
 function animate(){
     requestAnimationFrame(animate);
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+
+    objet2.rotation.y += 0.01;
+
+    objet1.rotation.y -= 0.01;
+
     controls.update();
     renderer.render(scene, camera);
 }
