c3ae2b37d142d992e16189888c2d9a29636523a5
julian marques <marquesjulian26@gmail.com>
2025-12-04 21:05:42 +0100

    os ok

---
+++ ndi_2025/public/js/apps/apps.js
@@ -0,0 +1,31 @@
+
+const apps = {
+    notepad: {
+        width: 500,
+        height: 400,
+        fullScreen: false,
+        title: "Bloc-notes",
+        link: "/truc/machine",
+        reduire: true,
+        resizable: true
+
+    },
+    browser: {
+        width: 800,
+        height: 500,
+        title: "Navigateur",
+        link: "/truc",
+        reduire: true,
+        resizable: true
+
+    },
+    discord: {
+        width: 900,
+        height: 600,
+        title: "Discord Web",
+        link: "https://discord.com",
+        reduire: true,
+        resizable: true
+
+    }
+};
