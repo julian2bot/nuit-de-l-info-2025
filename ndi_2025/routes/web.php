f578e7a5ca93bdffd01bad6dfd78b03451001eb4
julian marques <marquesjulian26@gmail.com>
2025-12-04 19:02:27 +0100

    init push avec info

--- ndi_2025/routes/web.php
+++ ndi_2025/routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('/truc', function () {
+    return view('truc.index');
+});
+
+Route::get('/truc/machine', function () {
+    return view('welcome');
+})->name("trucMachine");
