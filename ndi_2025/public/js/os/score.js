1a8711dc81cfebd579fc6b7fa45f7d6f36e8942b
julian marques <marquesjulian26@gmail.com>
2025-12-05 07:28:46 +0100

    test score

--- ndi_2025/public/js/os/score.js
+++ ndi_2025/public/js/os/score.js
@@ -1,10 +1,12 @@
+const csrf_token =  document.querySelector('meta[name="csrf-token"]').content;
+
 function sendScore(type, ref, nbpoint) {
     fetch("/score", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
             "Accept": "application/json",
-            "X-CSRF-TOKEN": document.querySelector('meta[name="csrf-token"]').content
+            "X-CSRF-TOKEN": csrf_token
         },
         body: JSON.stringify({
             type: type,
